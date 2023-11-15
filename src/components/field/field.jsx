6c6988d459e815b9eaefd8050e2a1306e808431b
LopezDev <aragonlopezjuan87@gmail.com>
2023-11-15 07:04:44 +0100

    create handleOnchage to input

--- src/components/field/field.jsx
+++ src/components/field/field.jsx
@@ -1,15 +1,16 @@
 import PropTypes from "react"
 import './field.css'
 
-export const Field = ({ label }) => {
+export const Field = ({ label, onChange }) => {
   return (
     <div className="field">
       <label>{label}</label>
-      <input type="number" value='2003' />
+      <input name={label} type="number" onChange={(e) => onChange(e)} />
     </div>
   )
 }
 
 Field.propTypes = {
-  label: PropTypes.string
+  label: PropTypes.string,
+  onChange: PropTypes.func
 }
--- src/container/ageForm/ageForm.jsx
+++ src/container/ageForm/ageForm.jsx
@@ -1,12 +1,26 @@
+import { useState } from 'react'
 import { Field } from '../../components'
 import './ageForm.css'
 
 export const AgeForm = () => {
+  const [ageUser, setAgeUser] = useState({
+    DIA: undefined,
+    MES: undefined,
+    AÑO: undefined,
+  })
+
+  const handleOnchange = (e) => {
+    setAgeUser({
+      ...ageUser,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
     <div className="ageForm">
-      <Field label='DIA' />
-      <Field label='MES' />
-      <Field label='AÑO' />
+      <Field label='DIA' onChange={handleOnchange} />
+      <Field label='MES' onChange={handleOnchange} />
+      <Field label='AÑO' onChange={handleOnchange} />
     </div>
   )
 }
